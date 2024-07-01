20a391b175d7db78fb2ca93a3c9191207f2f033c
ryd3v <hello@ryd3v.com>
2024-07-01 01:00:07 -0400

    add logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
         <nav className="bg-blue-500 dark:bg-zinc-800 text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <Link href="/">
+                <Link href="/" className="flex items-center">
+                    <Image src="/logo.png" alt="Logo" width={40} height={40} className="mr-2"/>
                     <p className="text-xl font-bold">Grocery Tracker</p>
                 </Link>
                 <div>
